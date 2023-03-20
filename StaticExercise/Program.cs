4c60dc0e4b97a602b33cda14de2860a7d00fd32d
Robert Rogers <rerogers@gmail.com>
2023-03-20 18:35:18 -0500

    updated Static Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("86 degree Fahrenheit converted to Celsius is: " + TempConverter.FahrenheitToCelsius(86));
+            Console.WriteLine("-132.237 degree Celsius converted to Fahrenheit is: " + TempConverter.CelsiusToFahrenheit(-132.237));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp)
+        {
+            return (5* (temp - 32) / 9);
+            //return ((5 / 9) * (temp - 32)); does not work, but why?
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            return (temp * 9 / 5 + 32);
+        }
+    }
+}
